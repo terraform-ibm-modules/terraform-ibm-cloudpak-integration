6afc47af8c2cd54e892921fe7303819316daf908
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-02-17 17:34:10 -0600

    removed entitlement key

--- test/cloudpak_integration_test.go
+++ test/cloudpak_integration_test.go
@@ -37,8 +37,8 @@ func TestAccIBMCP4I(t *testing.T) {
 			"owner"                = "terratest"
 			"roks_version"         = 4.7
 			"storage_class"        = "ibmc-file-gold-gid"
-			"entitled_registry_key" = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJJQk0gTWFya2V0cGxhY2UiLCJpYXQiOjE1OTY4MzcwMjUsImp0aSI6IjcwMDNkYmU0ZDczZjQ4Y2M4NmQ4Y2Q5ZWE0YzVlYmY4In0.62Llbq4dGKWhPWOngqBMz5SdMZdbnGYjOFlzmN7Fgvw"
-			"entitled_registry_user_email" = "ann.umberhocker@ibm.com"
+			"entitled_registry_key" = ""
+			"entitled_registry_user_email" = ""
 		},
 	})
 
